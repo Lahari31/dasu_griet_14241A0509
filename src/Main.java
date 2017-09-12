4ac65f05b73cbdb3000b274a77b8aac2c685de60
Lahari31 <31853386+Lahari31@users.noreply.github.com>
2017-09-12 13:18:28 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+Student st=new Student(Integer.parseInt(br.readLine(),br.readline,Integer.parseInt(br.readLine()),Date.parseDate(br.readLine));
+		StudentGroup sg=new StudentGroup(15);
+sg.getStudents();
+sg.setStudents(br.readLine());
+sg.getStudent(Integer.parseInt(br.readLine());
+sg.setStudent(br.readLine(),Integer.parseInt(br.readLine());
+
+
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	}
+
+}
